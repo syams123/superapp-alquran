4c4147ccfbc54a86e0de3e06f644636ab5945f09
syams123 <147891702+syams123@users.noreply.github.com>
2026-03-19 07:34:47 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,28 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+  },
+  // KODE PEMBUKA BLOKIR IFRAME
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.blogspot.com http://*.blogspot.com",
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOWALL',
+          },
+        ],
+      },
+    ]
+  },
 }
+
+module.exports = nextConfig
