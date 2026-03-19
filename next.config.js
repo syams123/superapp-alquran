a89183c65440b4cad6bc255bb4e7ba942b5bcb21
syams123 <147891702+syams123@users.noreply.github.com>
2026-03-19 07:37:45 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
 module.exports = {
+
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Frame-Options',
-            value: 'ALLOWALL',
-          },
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-ancestors *",
-          },
-        ],
-      },
-    ];
-  },
-};
+
+}
